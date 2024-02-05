d9680eb497327148004e9d0b901dfcb7a6678110
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-02-05 13:19:03 +0700

    CRUD FLUTTER

---
+++ json/lib/networking/display.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Display extends StatefulWidget {
+  Display({super.key});
+
+  @override
+  State<Display> createState() => _DisplayState();
+}
+
+class _DisplayState extends State<Display> {
+  // late Future<Album> album;
+  @override
+  //  void initState() {
+  //   super.initState();
+  //   album = NetworkManager().fetchAlbum();
+  // }
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- json/lib/user.dart
+++ json/lib/user.dart
@@ -12,13 +12,6 @@ class User {
   User(this.name, this.email, this.regisrationDateMilis);
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
-  // User.fromJson(Map<String, dynamic> json)
-  //     : name = json['name'],
-  //       email = json['email'];
 
   Map<String, dynamic> toJson() => _$UserToJson(this);
-  // Map<String, dynamic> toJson() => {
-  //       'name': name,
-  //       'email': email,
-  //     };
 }
